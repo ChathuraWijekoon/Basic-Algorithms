d5afaa45b57ccc1b1055eb6077684497532fb98c
Chathura B. Wijekoon <39031203+ChathuraWijekoon@users.noreply.github.com>
2022-05-06 12:49:49 +0530

    first commit

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_1;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Values(1,2));
+        Console.WriteLine(Values(2,3));
+        Console.WriteLine(Values(2,2));
+        Console.ReadLine();
+    }
+
+    public static int Values(int x, int y)
+    {
+        return x == y ? (x + y)*3 : x + y;
+    }
+}
---
+++ Exercise 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Diff(12));
+        Console.WriteLine(Diff(9));
+        Console.WriteLine(Diff(11));
+        Console.ReadLine();
+    }
+
+    public static int Diff(int n)
+    {
+        const int x = 10;
+
+        if (n>x)
+        {
+            return(n-x)*3;
+        }
+        return x-n;
+    }
+}

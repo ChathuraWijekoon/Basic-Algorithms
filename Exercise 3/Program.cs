18c3cc3f966492755ea7cf242e844b463f95d4c5
Chathura B. Wijekoon <39031203+ChathuraWijekoon@users.noreply.github.com>
2022-05-06 20:25:27 +0530

    Final

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Exercise_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Answer(30,0));
+        Console.WriteLine(Answer(25,5));
+        Console.WriteLine(Answer(20,30));
+        Console.WriteLine(Answer(20,25));
+        Console.ReadLine();
+    }
+
+    public static bool Answer(int x, int y)
+    {
+        return x == 30 || y == 30 || (x + y == 30);
+    }
+}
+

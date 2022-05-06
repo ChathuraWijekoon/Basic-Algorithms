1b1e105022ada78e363f43b0b4e33659764717ba
Chathura B. Wijekoon <39031203+ChathuraWijekoon@users.noreply.github.com>
2022-05-06 23:07:43 +0530

    final

---
+++ Exercise 6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Exercise_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Output("Python",1));
+        Console.WriteLine(Output("Python",0));
+        Console.WriteLine(Output("Python",4));
+    }
+
+    public static string Output(String s, int n)
+    {
+        return s.Remove(n,1);
+    }
+}

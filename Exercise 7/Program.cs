9d2909337589cc0eb5196a6ac5268bcb8ab31d8a
Chathura B. Wijekoon <39031203+ChathuraWijekoon@users.noreply.github.com>
2022-05-06 23:54:37 +0530

    final

---
+++ Exercise 7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Exercise_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Swap("abcd"));
+        Console.WriteLine(Swap("xy"));
+    }
+
+    public static string Swap(string s)
+    {
+        return s.Length > 1 ? s.Substring(s.Length - 1) + s.Substring(1, s.Length - 2) + s.Substring(0,1) : s;
+    }
+}

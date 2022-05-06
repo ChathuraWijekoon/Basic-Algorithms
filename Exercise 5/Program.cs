38f1ca28fce572bbeec2846841c8cd8cc64856bd
Chathura B. Wijekoon <39031203+ChathuraWijekoon@users.noreply.github.com>
2022-05-06 22:50:19 +0530

    final program

---
+++ Exercise 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Exercise_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Output("if else"));
+        Console.WriteLine(Output("else"));
+        Console.ReadLine();
+    }
+
+    public static string Output(string s)
+    {
+        if (s.Length > 2 && s.Substring(0,2).Equals("if"))
+        {
+            return s;
+        }
+        return "if " + s;
+    }
+}

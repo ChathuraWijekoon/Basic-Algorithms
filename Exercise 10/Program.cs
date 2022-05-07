f09416cb0c4f05df34265e1b481aa316e1616419
Chathura B. Wijekoon <39031203+ChathuraWijekoon@users.noreply.github.com>
2022-05-07 19:57:10 +0530

    final commit

---
+++ Exercise 10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Exercise_10;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine(Value(1,2,20));
+    }
+
+    public static int Value(int x, int y, int z)
+    {
+        var max = Math.Max(x, Math.Max(y,z));
+        return max;
+    }
+}

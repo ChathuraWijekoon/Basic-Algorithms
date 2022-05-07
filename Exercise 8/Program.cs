84e7b480d626d5e76a6ae6fc0c57e3f698087d7c
Chathura B. Wijekoon <39031203+ChathuraWijekoon@users.noreply.github.com>
2022-05-07 19:16:19 +0530

    final

---
+++ Exercise 8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Exercise_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Answer(15));
+        Console.WriteLine(Answer(20));
+        Console.WriteLine(Answer(14));
+    }
+
+    public static bool Answer(int n)
+    {
+        return n % 3 == 0 || n % 7 == 0;
+    }
+}
---
+++ Exercise 9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Exercise_9;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine(Answer("C# DOTNET"));
+        Console.WriteLine(Answer("C++"));
+    }
+
+    public static bool Answer(string str)
+    {
+        return (str.Length < 3 && str.Equals("C#")) || (str.StartsWith("C#") && str[2] == ' ');
+    }
+}
